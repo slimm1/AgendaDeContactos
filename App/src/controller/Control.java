2f5a96393ff5f067602261b23e0c0a5073d58cb8
Vespertino <Vespertino@C09PC07.informatica.comercio>
2023-09-29 19:41:40 +0200

    Introducidos cambios en la clase control y contacto

--- App/src/controller/Control.java
+++ App/src/controller/Control.java
@@ -3,17 +3,67 @@ package controller;
 import model.Contact;
 
 import java.io.*;
-import java.lang.reflect.Array;
-import java.util.HashSet;
-
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.TreeSet;
 public class Control {
-    public void alta(){
-
+    private final TreeSet<Contact> bufferList, original;
+    private final File inputFile;
+    public Control(String routing){
+        inputFile = new File(routing);
+        bufferList = readFile();
+        original = readFile();
     }
-    public HashSet<Contact> readFile(File file){
+    private void addContact(String name, int number){
+        bufferList.add(new Contact(name, number));
+    }
+    private void removeContact(String name){
+        Contact c = getContactByName(name);
+        if(c != null){
+            bufferList.remove(c);
+        }
+        else{
+            System.out.println("El contacto introducido no figura en la lista!");
+        }
+    }
+    private void modifyContact(String name, String newName, int newNumber){
+        Contact c = getContactByName(name);
+        if(c != null){
+            c.setName(newName);
+            c.setNumber(newNumber);
+        }
+        else{
+            System.out.println("El contacto introducido no figura en la lista!");
+        }
+    }
+    private String listContacts(){
+        StringBuilder out = new StringBuilder();
+        for(Contact c: bufferList){
+            out.append(c.toString());
+            out.append(System.getProperty("line.separator"));
+        }
+        return out.toString();
+    }
+    private void saveChanges(){
+        System.out.println(new File("../versions").mkdir()?"created new versions dir":"versions dir already exists");
+        rewriteFile(new File("../versions/"+getDateTime()+inputFile.getName()), original);
+        rewriteFile(new File(inputFile.getPath()), bufferList);
+    }
+    private Contact getContactByName(String name){
+        for(Contact c: bufferList){
+            if(c.getName().equalsIgnoreCase(name)){
+                return c;
+            }
+        }
+        return null;
+    }
+    public String getDateTime(){
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("ddLLLuuuu-HHmmss"));
+    }
+    private TreeSet<Contact> readFile(){
         try{
-            ObjectInputStream input = new ObjectInputStream(new FileInputStream(file));
-            HashSet <Contact> contactList = new HashSet<>();
+            ObjectInputStream input = new ObjectInputStream(new FileInputStream(inputFile));
+            TreeSet <Contact> contactList = new TreeSet<>();
             Contact newContact = (Contact) input.readObject();
             while(newContact!=null){
                 contactList.add(newContact);
@@ -35,7 +85,7 @@ public class Control {
             return null;
         }
     }
-    public void rewriteFile(File file, HashSet<Contact> contactList){
+    private void rewriteFile(File file, TreeSet<Contact> contactList){
         try {
             ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(file));
             for(Contact c:contactList){
--- App/src/model/Contact.java
+++ App/src/model/Contact.java
@@ -1,8 +1,10 @@
 package model;
 
+import java.io.Serializable;
+import java.util.Comparator;
 import java.util.Objects;
 
-public class Contact {
+public class Contact implements Comparable<Contact>, Serializable {
     public String name;
     public int number;
     public Contact(String name, int number){
@@ -23,7 +25,7 @@ public class Contact {
         this.name = name;
     }
 
-    public void settNumber(int number) {
+    public void setNumber(int number) {
         this.number = number;
     }
 
@@ -31,9 +33,15 @@ public class Contact {
         if(obj==null){return false;}
         if(!(obj instanceof Contact)){return false;}
         Contact c = (Contact)obj;
-        return c.getName().equalsIgnoreCase(this.name) || c.getNumber() == this.number;
+        return c.getName().equalsIgnoreCase(this.name);
     }
     public int hashCode() {
-        return Objects.hash(name,number);
+        return Objects.hash(name);
+    }
+    public int compareTo(Contact c) {
+        return name.compareTo(c.getName());
+    }
+    public String toString() {
+        return this.name + " --> " + number;
     }
 }
